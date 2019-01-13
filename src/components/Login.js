f991bca9232f88dbbccaf2f9d1cdb49ee69c9b0e
Juliano Silva <juliano.silva2011@gmail.com>
2019-01-13 01:36:12 -0200

    add notifications on login and register pages

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import { Box, Form, FormField, Button, Anchor } from 'grommet';
 
+import Notification from './Notification';
+
 import { connect } from 'react-redux';
 
 import { login } from '../actions/auth';
 
 class Login extends Component {
+  state = {
+    notificationOpen: false,
+    notificationMessage: '',
+    notificationError: false,
+  }
+
   handleSubmit = event => {
     this.props.login(event.value).then(
       (res) => this.props.history.push('/dashboard'),
-    ).catch(err => alert(err.response.data.error));;
+    ).catch(err => this.showNotification.bind(this)(err.response.data.error, true));
+  }
+
+  showNotification = (notificationMessage, error = false) => {
+    this.setState({ 
+      notificationMessage,
+      notificationOpen: true,
+      notificationError: error
+    });
+    
+    setTimeout(() => {
+      this.closeNotification();
+    }, 5000);
+  }
+
+  closeNotification = () => {
+    this.setState({ notificationOpen: false });
   }
 
   render() {
+    const { notificationOpen, notificationMessage, notificationError } = this.state;
+
     return (
       <Box align='center' margin={{ top: '100px' }} wrap={true}>
         <h1>Ekki - Login</h1>
@@ -22,6 +48,12 @@ class Login extends Component {
           <Button type='submit' label='Log In'/>
           <Anchor onClick={ () => { this.props.history.push('/register'); } } label='Register' margin={{left: '25px'}} />
         </Form>
+        {notificationOpen && 
+          <Notification 
+            message={notificationMessage}
+            error={notificationError}
+            onClose={this.closeNotification.bind(this)} />
+        }
       </Box>
     );
   }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import { Box, Form, FormField, Button, Anchor } from 'grommet';
 
+import Notification from './Notification';
+
 import { connect } from 'react-redux';
 
 import { register } from '../actions/auth';
 
 class Register extends Component {
+  state = {
+    notificationOpen: false,
+    notificationMessage: '',
+    notificationError: false,
+  }
+
   handleSubmit = event => {
     this.props.register(event.value).then(
       (res) => this.props.history.push('/dashboard'),
-    ).catch(err => alert(err.response.data.error));;
+    ).catch(err => this.showNotification.bind(this)(err.response.data.error, true));
+  }
+
+  showNotification = (notificationMessage, error = false) => {
+    this.setState({ 
+      notificationMessage,
+      notificationOpen: true,
+      notificationError: error
+    });
+    
+    setTimeout(() => {
+      this.closeNotification();
+    }, 5000);
+  }
+
+  closeNotification = () => {
+    this.setState({ notificationOpen: false });
   }
 
   render() {
+    const { notificationOpen, notificationMessage, notificationError } = this.state;
+
     return (
       <Box align='center' margin={{ top: '100px' }} wrap={true}>
         <h1>Ekki - Register</h1>
@@ -23,6 +49,12 @@ class Register extends Component {
           <Button type='submit' label='Submit'/>
           <Anchor onClick={ () => { this.props.history.push('/login'); } } label='Back to Login' margin={{left: '25px'}} />
         </Form>
+        {notificationOpen && 
+          <Notification 
+            message={notificationMessage}
+            error={notificationError}
+            onClose={this.closeNotification.bind(this)} />
+        }
       </Box>
     );
   }
